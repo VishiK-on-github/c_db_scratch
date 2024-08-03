3b7cc79da7bf0aa0447f5fc8d26ab6fcce5da522
Vishwanath Kamath <vck281000@gmail.com>
2024-08-03 16:12:19 +0530

    sorted insertion and duplicate keys done

--- db.c
+++ db.c
@@ -16,7 +16,7 @@ typedef struct {
 } InputBuffer;
 
 // defines types for statement execution
-typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL } ExecuteResult;
+typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL, EXECUTE_DUPLICATE_KEY } ExecuteResult;
 
 // defines types for meta command
 typedef enum {
@@ -143,12 +143,28 @@ void *leaf_node_value(void *node, uint32_t cell_num) {
   return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
 }
 
+// this method is used to setup type of node
+void set_node_type(void * node, NodeType type) {
+  uint8_t value = type;
+  *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
+}
+
 // this method is used to initialize a node
-void initialize_leaf_node(void *node) { *leaf_node_num_cells(node) = 0; }
+void initialize_leaf_node(void *node) {
+  set_node_type(node, NODE_LEAF); 
+  *leaf_node_num_cells(node) = 0;
+}
 
 // utility to print row using select statement
 void print_row(Row *row) {
   printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
+
+// this method is used to get type of node
+// if it is a leaf or internal node
+NodeType get_node_type(void * node) {
+  uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+  return (NodeType)value;
 }
 
 // copies from source to pages
@@ -220,6 +236,37 @@ void *get_page(Pager *pager, uint32_t page_num) {
   return pager->pages[page_num];
 }
 
+// returns the cursor to the location of where row is 
+// or where it must be incase we dont find it
+Cursor * leaf_node_find(Table * table, uint32_t page_num, uint32_t key) {
+  void * node = get_page(table->pager, page_num);
+  uint32_t num_cells = *leaf_node_num_cells(node);
+
+  Cursor * cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->page_num = page_num;
+
+  uint32_t min_index = 0;
+  uint32_t one_past_max_index = num_cells;
+
+  while (min_index != one_past_max_index) {
+    uint32_t index = (min_index + one_past_max_index) / 2;
+    uint32_t key_at_index = *leaf_node_key(node, index);
+    if (key == key_at_index) {
+      cursor->cell_num = index;
+      return cursor;
+    }
+    if (key < key_at_index) {
+      one_past_max_index = index;    
+    } else {
+      min_index = index + 1;
+    }
+  }
+
+  cursor->cell_num = min_index;
+  return cursor;
+}
+
 // this method is used to initialize a cursor
 // at the 0th row of the table
 Cursor *table_start(Table *table) {
@@ -238,19 +285,18 @@ Cursor *table_start(Table *table) {
   return cursor;
 }
 
-// this method is used to position the cursor to the end of the table
-Cursor *table_end(Table *table) {
-  Cursor *cursor = malloc(sizeof(Cursor));
+// returns the position for a given key
+// if no key, it gives us the position where it should be inserted
+Cursor * table_find(Table * table, uint32_t key) {
+  uint32_t root_page_num = table->root_page_num;
+  void * root_node = get_page(table->pager, root_page_num);
 
-  cursor->table = table;
-  cursor->page_num = table->root_page_num;
-
-  void *root_node = get_page(table->pager, table->root_page_num);
-  uint32_t num_cells = *leaf_node_num_cells(root_node);
-  cursor->cell_num = num_cells;
-  cursor->end_of_table = true;
-
-  return cursor;
+  if (get_node_type(root_node) == NODE_LEAF) {
+    return leaf_node_find(table, root_page_num, key);
+  } else {
+    printf("Need to implement searching an internal node\n");
+    exit(EXIT_FAILURE);
+  }
 }
 
 // this method is used to see row fits in which page of the table
@@ -557,13 +603,23 @@ PrepareResult prepare_statement(InputBuffer *input_buffer,
 // this method is used to insert rows into table
 ExecuteResult execute_insert(Statement *statement, Table *table) {
   void *node = get_page(table->pager, table->root_page_num);
-  if ((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS)) {
+  uint32_t num_cells = (*leaf_node_num_cells(node));
+  if (num_cells >= LEAF_NODE_MAX_CELLS) {
     return EXECUTE_TABLE_FULL;
   }
 
   Row *row_to_insert = &(statement->row_to_insert);
-  // we create a cursor to the current last row to insert a new record
-  Cursor *cursor = table_end(table);
+
+  uint32_t key_to_insert = row_to_insert->id;
+  // 
+  Cursor * cursor = table_find(table, key_to_insert);
+
+  if (cursor->cell_num < num_cells) {
+    uint32_t key_at_index = *leaf_node_key(node, cursor->cell_num);
+    if (key_at_index == key_to_insert) {
+      return EXECUTE_DUPLICATE_KEY;
+    }
+  }
 
   leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
 
@@ -656,6 +712,9 @@ int main(int argc, char *argv[]) {
     switch (execute_statement(&statement, table)) {
       case EXECUTE_SUCCESS:
         printf("Executed.\n");
+        break;
+      case (EXECUTE_DUPLICATE_KEY):
+        printf("Error: Duplicate key.\n");
         break;
       case EXECUTE_TABLE_FULL:
         printf("Error: Table full.\n");
